26e69a81490de70dd7444e3bca0e51062adc5478
Dominguezd01 <migueldominguezroman01@gmail.com>
2023-11-28 21:09:21 +0100

    some useless changes

--- src/routes/users/auth/register/+page.svelte
+++ src/routes/users/auth/register/+page.svelte
@@ -1,20 +1,23 @@
 <div class="grid items-center justify-center m-14">
-  <form class="grid items-center gap-4 grid-cols-2 text-center">
-    <div class="grid items-center">
-      <label for="email">Email</label>
-      <input type="text" id="email" class="w-36 text-center" />
+  <form class="grid items-center">
+    <div class="grid items-center grid-cols-2 gap-4 text-center m-4">
+      <div class="grid items-center">
+        <label for="email">Email</label>
+        <input type="text" id="email" class="w-36 text-center" />
+      </div>
+      <div class="grid items-center">
+        <label for="email">Username</label>
+        <input type="text" id="userName" class="w-36 text-center" />
+      </div>
+      <div class="grid items-center">
+        <label for="email">Password</label>
+        <input type="password" id="password" class="w-36 text-center" />
+      </div>
+      <div class="grid items-center">
+        <label for="email">Email</label>
+        <input type="text" id="email" class="w-36 text-center" />
+      </div>
     </div>
-    <div class="grid items-center">
-      <label for="email">Email</label>
-      <input type="text" id="email" class="w-36 text-center" />
-    </div>
-    <div class="grid items-center">
-      <label for="email">Email</label>
-      <input type="text" id="email" class="w-36 text-center" />
-    </div>
-    <div class="grid items-center">
-      <label for="email">Email</label>
-      <input type="text" id="email" class="w-36 text-center" />
-    </div>
+    <button>SEND!</button>
   </form>
 </div>
